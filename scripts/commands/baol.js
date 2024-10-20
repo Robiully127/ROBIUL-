2cf31945f353b93895c980fb8c67bb9a69f58291
Robiully127 <rislamsohid@gmail.com>
2024-10-20 20:27:03 +0600

    Create baol.js

---
+++ scripts/commands/baol.js
@@ -0,0 +1,57 @@
+module.exports.config = {
+  name: "baol",
+  version: "1.0.0",
+  permission: 0,
+  credits: "ROBIUL",
+  description: "",
+  prefix: true, 
+  category: "no prefix", 
+  usages: "user",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+ 
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  if (event.body.indexOf("baol") == 0) {
+    const axios = global.nodemodule["axios"];
+    const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    var link = ["https://i.imgur.com/8n11KYQ.mp4"];
+    var callback = () => api.sendMessage({
+      body: `baol`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    const timeStart = Date.now();
+    const PREFIX = config.PREFIX;
+    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
+  }
+};
+ 
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+};
+ 
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  let { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["baol"] == "undefined" || data["baol"] == true) data["baol"] = false;
+  else data["baol"] = true;
+  await Threads.setData(threadID, {
+    data
+  });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["baol"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
